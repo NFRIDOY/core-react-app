ef2ff1284c2402031b69fec98d4a1e80e31ac5d0
NF RIDOY <nfridoy@gmail.com>
2023-06-16 13:14:00 +0600

    card bootstrap

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import ReactDefault from "./components/ReactDefault";
-import CardDark from "./components/CardDark";
+// import CardDark from "./components/CardDark";
+// import Card from "./components/card";
+import Card1 from "./components/Cardx";
+
 
 function App() {
 	return (
 		<>
 			<Navbar />
-			{/* <div className="ReactDefaultS">
+			<div className="ReactDefaultS">
 				<ReactDefault />
 				<ReactDefault />
-			</div> */}
+			</div>
 			<div>
 				<h1>Do Some Practice</h1>
 				<p>Let's Sum some Numbers, 2 + 3 = {2 + 3}</p>
 			</div>
-			<CardDark  />
+			{/* <CardDark  /> */}
+			<div className="d-flex">
+				<Card1 />
+				<Card1 />
+			</div>
 		</>
 	);
 }
